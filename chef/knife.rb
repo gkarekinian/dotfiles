757d7036dad2e3039a7c516b2c7e04cac204adac
Grégory Karékinian <greg@karekinian.com>
2012-06-12 11:45:47 +0200

    Renamed generic chef folder

--- chef/knife.rb
+++ chef/knife.rb
@@ -1,10 +1,10 @@
 current_dir = File.dirname(__FILE__)
-user        = ENV['OPSCODE_USER'] || ENV['USER']
+user = ENV['OPSCODE_USER'] || ENV['USER']
 
 log_level                :info
 log_location             STDOUT
 node_name                user
-client_key               "#{current_dir}/#{user}.pem"
+client_key               "#{current_dir}/#{ENV['ORGNAME']}-#{user}.pem"
 validation_client_name   "#{ENV['ORGNAME']}-validator"
 validation_key           "#{current_dir}/#{ENV['ORGNAME']}-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/#{ENV['ORGNAME']}"
